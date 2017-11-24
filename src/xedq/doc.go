9d66ccd5601627fe43c5919e8bc10862980594fd
quasilyte <quasilyte@gmail.com>
2017-11-24 21:51:56 +0300

    add doc.go

---
+++ src/xedq/doc.go
@@ -0,0 +1,8 @@
+// Package xedq brings Intel XED powers into Go space.
+//
+// Convenient library built on top of Intel XED for encoding/decoding of X86 instructions.
+//
+// Focused around two use cases:
+//   1. Manually-coded expressions: succinct and readable API, easy to use.
+//   2. Programmatically built expressions: composable API, easy to extend.
+package xedq
